c5b1607c4e8010fea5932f641fea68b85203f6a7
Vlad.Gerasimov <wolsenpak1@gmail.com>
2025-01-29 13:00:59 +0300

    Lesson 1

---
+++ HomeWorkApp/src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        printThreeWords();
+        checkSumSign(1,2);
+        printColor(101);
+        compareNumbers(3,2);
+    }
+
+    public static void printThreeWords() {
+//        System.out.println("Orange");
+//        System.out.println("Banana");
+//        System.out.println("Apple");
+    }
+
+    public static void checkSumSign (int a, int b ) {
+//        if (a + b >= 0)
+//        System.out.println("Сумма положительная");
+//        else System.out.println("Сумма отрицательная");
+    }
+
+    public static void printColor(int value) {
+//        if  (value <= 0) {
+//            System.out.print("Красный");
+//        }
+//            else if (value > 0 && value <= 100) {
+//                System.out.print("Желтый");
+//            }
+//            else {
+//                System.out.println("Зеленый");
+//            }
+   }
+
+   public static void compareNumbers (int a, int b) {
+//        if (a >= b)
+//            System.out.println(a + " >= " + b);
+//            else System.out.println(a + " < " + b);
+   }
+}
+
