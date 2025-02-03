7c3eaa2c6b66163303b57e289acceea30a8aeb74
Vlad.Gerasimov <wolsenpak1@gmail.com>
2025-02-03 19:03:25 +0300

    Lesson 2

---
+++ HomeWorkApp/src/Lesson 2/java/SecondHomeWork.java
@@ -0,0 +1,44 @@
+public class SecondHomeWork {
+    public static void main(String[] args) {
+        System.out.println(firstTask(20, 10));
+        secondTask(-10);
+        System.out.println(thirdTask(-10));
+        fourTask("Привет", 3);
+        System.out.println(theFifthTask(100));
+    }
+
+    public static boolean firstTask(int a, int b) {
+        int sum = a + b;
+        return sum > 10 && sum <= 20;
+    }
+
+    public static void secondTask(int a) {
+        if (a >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    public static boolean thirdTask(int a) {
+        if (a < 0) {
+            return true;
+        }
+            return false;
+
+    }
+
+    public static void fourTask (String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
+        }
+    }
+
+    public static boolean theFifthTask(int year) {
+        if ((year % 4 == 0) && (year % 100 != 0) || year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
